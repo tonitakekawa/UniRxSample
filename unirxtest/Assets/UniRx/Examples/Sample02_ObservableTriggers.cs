bb6a29f95be46d8924fe6c8a4d20bae17ce6cf9a
Toni Takekawa <tonitakekawa@gmail.com>
2015-05-17 01:12:14 +0900

    first commit

---
+++ unirxtest/Assets/UniRx/Examples/Sample02_ObservableTriggers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Sample02_ObservableTriggers : MonoBehaviour
+{
+    void Start()
+    {
+        this.UpdateAsObservable()
+            .SampleFrame(120)
+            .Subscribe(_ => Debug.Log("hoge"));
+    }
+}
